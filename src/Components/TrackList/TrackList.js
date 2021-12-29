c3f7722df3cee8275e9bfcc261ac1cf6a8eaedca
Liberty <72211604+lordmatic@users.noreply.github.com>
2021-12-29 12:48:53 +0000

    App updates

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './TrackList.css';
+
+import Track from '../Track/Track';
+
+class TrackList extends React.Component {
+    render() {
+        return (
+             <div class="TrackList"> 
+            {    
+         //  step 34 brakes the code //
+         /*  this.props.tracks.map(track => {
+              return <Track track={track}
+              key={track.id} 
+              onAdd={this.props.onAdd}  />
+            })   */
+              }   
+                         
+   
+</div> 
+        )
+    }
+}
+export default TrackList; 
